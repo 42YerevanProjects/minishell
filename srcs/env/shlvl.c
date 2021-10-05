59aa7c7f1b83e7e241da26e94f2b00c0d39734a6
Nathanael <sargishovsepyan2001@gmail.com>
2021-10-05 14:46:53 -0400

    Enhanced by making the method shorter

--- srcs/env/shlvl.c
+++ srcs/env/shlvl.c
@@ -1,28 +1,7 @@
 #include "../../includes/minishell.h"
 
-static char	*get_env_name(char *value)
-{
-	char	*env_name;
-	int		size;
-	int		i;
-
-	i = 0;
-	while (value[i] != '=')
-		i++;
-	size = i;
-	env_name = malloc(size + 1);
-	i = 0;
-	while (i < size)
-	{
-		env_name[i] = value[i];
-		i++;
-	}
-	return env_name;
-}
-
 void	increment_shell_level(t_env *env)
 {
-	char	*env_name;
 	char	*shlvl_value;
 	int		shlvl;
 	t_env	*head;
@@ -34,11 +13,9 @@ void	increment_shell_level(t_env *env)
 	shlvl = ft_atoi(shlvl_value) + 1;
 	while (head && head->next)
 	{
-		env_name = get_env_name(head->value); 
-		if (ft_strcmp("SHLVL", env_name) == 0)
+		if (ft_strncmp("SHLVL", head->value, 5) == 0)
 		{
 			head->value = ft_strjoin("SHLVL=", ft_itoa(shlvl));
-			ft_memdel(env_name);
 			return ;
 		}
 		head = head->next;
