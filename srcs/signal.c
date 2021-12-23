d91fb2f6f10101e20e338c850cc9e33f6fc09117
Nathanael <sargishovsepyan2001@gmail.com>
2021-12-23 08:04:51 -0500

    Updated by putting suppress_output function t the top

--- srcs/signal.c
+++ srcs/signal.c
@@ -2,16 +2,6 @@
 
 /* UTILS FOR SIGNALS */
 
-static void	reprompt(int sig)
-{
-	(void)sig;
-	suppress_output();
-	write(1, "\n", 1);
-	rl_on_new_line();
-	rl_replace_line("", 0);
-	rl_redisplay();
-}
-
 static void	suppress_output(void)
 {
 	struct termios	termios_p;
@@ -23,6 +13,16 @@ static void	suppress_output(void)
 		perror("minishell: tcsetattr");
 }
 
+static void	reprompt(int sig)
+{
+	(void)sig;
+	suppress_output();
+	write(1, "\n", 1);
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+}
+
 static void	sigquit(int sig)
 {
 	(void)sig;
@@ -31,15 +31,14 @@ static void	sigquit(int sig)
 	rl_redisplay();
 }
 
-static void	interrupt(int sig)
-{
-	(void)sig;
-	suppress_output();
-	rl_on_new_line();
-	rl_redisplay();
-	exit(1);
-}
-
+//static void	interrupt(int sig)
+//{
+//	(void)sig;
+//	suppress_output();
+//	rl_on_new_line();
+//	rl_redisplay();
+//	exit(1);
+//}
 
 /* SIGNAL FUNCTIONS */
 
