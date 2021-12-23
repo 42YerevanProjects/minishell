aa9f32adcd169b3952212eabfcf836ef4f33b282
Nathanael <sargishovsepyan2001@gmail.com>
2021-12-23 07:29:13 -0500

    Put increment_shell_level in init function

--- srcs/main.c
+++ srcs/main.c
@@ -2,12 +2,13 @@
 
 int main(int argc, char **argv, char **env)
 {
+	t_mini	mini;
 	char	*line;
 
 	(void) argc;
 	(void) argv;
-	init(&g_mini, env);
-	increment_shell_level(g_mini.env);
+	init(&mini, env);
+	sig_init();
 	while (1)
 	{
 		line = readline("\033[0;36m\033[1m minishell ▸ \033[0m");
