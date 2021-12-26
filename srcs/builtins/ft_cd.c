f8eb65718f811486b34724a00da2e47504d38c64
Nathanael <sargishovsepyan2001@gmail.com>
2021-12-26 09:25:02 -0500

    Added ft_change_dir method

--- srcs/builtins/ft_cd.c
+++ srcs/builtins/ft_cd.c
@@ -1,6 +1,37 @@
 #include "../../includes/minishell.h"
 
-int	ft_cd(t_cmd *cmd)
+static int	update_env_wds(t_mini *mini, char *oldpwd, int status)
+{
+	//TODO: Update PWD and OLDPWD after doing export
+}
+
+static int	ft_change_dir(t_cmd *cmd, int *status, char *oldpwd)
+{
+	/* if it is "cd -" go to oldpwd */
+	if (cmd->args[1][0] == '-' && cmd->args[1][1] == '\0')
+	{
+		if (ft_getenv("OLDPWD"))
+		{
+			*status = ft_abs(chdir(ft_getenv("OLDPWD")));
+			if (!*status)
+			{
+				ft_putstr_fd(ft_getenv("OLDPWD"), cmd->out);
+				ft_putstr_fd("\n", cmd->out);
+			}
+		}
+		else
+		{
+			free(oldpwd);
+			return (ft_error("cd", "OLDPWD not set"));
+		}
+	}
+	/* else if a path is specified */
+	else
+		*status = ft_abs(chdir(cmd->args[1]));
+	return (0);
+}
+
+int	ft_cd(t_mini *mini, t_cmd *cmd)
 {
 	int		status;
 	char	*oldpwd;
@@ -27,7 +58,10 @@ int	ft_cd(t_cmd *cmd)
 	/* perform the change of the directory */
 	else
 	{
-		//TODO: deal with cd
+		if (ft_change_path(cmd, &status, oldpwd))
+			return (1);
 	}
-
+	/* update PWD and OLDPWD environment variables */
+	return (update_env_wds(mini, oldpwd, status));
 }
+
