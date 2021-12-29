c3598d018194727732e8d66b1814ce251336bc4e
Nathanael <sargishovsepyan2001@gmail.com>
2021-12-29 06:26:52 -0500

    Added comments

--- srcs/parse/extract_token.c
+++ srcs/parse/extract_token.c
@@ -17,9 +17,9 @@ static int	treat_pipe_and_redirection(char *line, char **token, t_expansion *exp
 	{
 		if (!ft_strncmp(&line[exp->j], "<<", 2) || !ft_strncmp(&line[exp->j],
 				">>", 2))
-			exp->j++;
-		ft_append_token(token, &line[exp->i], exp->j - exp->i + 1, exp->expand);
-		return (exp->j + 1);
+			exp->j += 2;
+		ft_append_token(token, &line[exp->i], exp->j - exp->i, exp->expand);
+		return (exp->j);
 	}
 }
 
@@ -55,6 +55,67 @@ static void	token_init(char *line, char **token, char **quote, t_expansion *exp)
 }
 
 /**
+ * The functions processes a quote that has been encountered. The function
+ * returns 1 if closing quote has been encountered, 0 otherwise.
+ **/
+
+static int	process_token_quote(char *line, char **token, char **quote, t_expansion *exp)
+{
+	char	*tmp;	//
+
+	/* Try to find the closing quote */
+	tmp = ft_strchr(&line[exp->j + 1], line[exp->j]);
+	if (tmp)
+	{
+		if (!**quote)
+			ft_append_token(quote, "\'", 1, 0);
+		/* Add the everything before the quotes to the token */
+		ft_append_token(token, &line[exp->i], exp->j - exp->i, exp->expand);
+		exp->i = exp->j + 1;		// Update the start index to the character after opening quote
+		exp->j = (tmp - 1) - line;	// Go to the closing quote  
+		/* No need for expansion if single quotes */
+		if (line[exp->j + 1] == '\'')
+			exp->expand = 0;
+		/* Append the word in quotes */
+		ft_append_token(token, &line[exp->i], exp->j - exp->i + 1, exp->expand);
+		exp->expand = 1;		// Reset expansion flag
+		exp->i = exp->j + 2;	// Update starting index to point after closing quote
+		exp->j = exp->i;		
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * The functions checks the character and decides what to do with it. The
+ * function also treats opening and closing quotes. Function returns 1 if
+ * quote has been encountered, 0 otherwise
+ **/
+
+static int	process_token_char(char *line, char **token, char **quote, t_expansion *exp)
+{
+	int		ret;
+
+	/* If character is a normal character (no quotes) */
+	if (line[exp->j] != '\'' && line[exp->j] != '\"'
+		&& !ft_isspace(line[exp->j + 1]) && line[exp->j + 1])
+			;
+	/* If it is a closing quote */
+	else if ((ft_isspace(line[exp->j + 1]) && !(line[exp->j] == '\''
+				|| line[exp->j] == '\"')) || !line[exp->j + 1])
+		ft_append_token(token, &line[exp->i], exp->j - exp->i + 1, exp->expand);
+	/* If it is an openning quote */
+	else
+	{
+		ret = process_token_quote(line, token, quote, exp);
+		if (ret)
+			return (1);
+	}
+	return (0);
+}
+
+
+/**
  * The function takes a line and refferences of token and quote variables.
  * It initializes the token and quote variables charachter by character appropriatly.
  * Deals with expansions, pipes and redirections. It initializes one token at a time.
@@ -64,15 +125,21 @@ static void	token_init(char *line, char **token, char **quote, t_expansion *exp)
 int	extract_token(char *line, char **token, char **quote)
 {
 	t_expansion	exp;									// struct to help deal with expansions
+	int			cont;									// stores the status after processing a char
 
 	token_init(line, token, quote, &exp);				// initializes token, quote and exp
 	/* Iterates over the characters of the given word */
 	while (!ft_isspace(line[exp.j]) && line[exp.j])
 	{
-		/* If the character is a redirection or pipe append to token and stop */
+		/* If the character is a redirection or pipei, append to token and stop */
 		if (line[exp.j] == '<' || line[exp.j] == '>' || line[exp.j] == '|')
 			return (treat_pipe_and_redirection(line, token, &exp));
-		return 0;		
+		/*  Else check for quote characters */
+		cont = process_token_char(line, token, quote, &exp);
+		/* If quotes has been encountered then exp.j has been updated */
+		if (cont)
+			continue ;
+		exp.j++;		
 	}
 	return (exp.j);
 }
--- srcs/parse/parse_and_execute.c
+++ srcs/parse/parse_and_execute.c
@@ -1,8 +1,12 @@
 #include "../../includes/minishell.h"
 
+/**
+ * The function is supposed to parse the input and execute the commands.
+ **/
+
 void	parse_and_execute(char *line)
 {
-	/* initializes the tokens of struct g_data */
+	/* initializes the tokens of struct g_mini */
 	get_tokens(line);
 	//TODO: deal with the rest of the parsing
 	
