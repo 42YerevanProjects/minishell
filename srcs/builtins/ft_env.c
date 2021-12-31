a53866d6afeda47e35bdadfbf96153d13645be97
Nathanael <sargishovsepyan2001@gmail.com>
2021-12-31 12:11:42 -0500

    A function that mimics the beavior of the command env in bash.

---
+++ srcs/builtins/ft_env.c
@@ -0,0 +1,24 @@
+#include "../../includes/minishell.h"
+
+/**
+ * The function mimics the behavior of the env in bash. Returns 0
+ * on success, 1 otherwise.
+ **/
+
+int	ft_env(t_cmd *cmd)
+{
+	t_env	*head;
+
+	head = g_mini.env;
+	if (ft_matrixlen(cmd->args) > 1)
+	{
+		ft_minishell_error("minishell: env: invalid number of args to env");
+		return (1);
+	}
+	while (head)
+	{
+		ft_putstr_fd(head->value, cmd->out);
+		head = head->next;
+	}
+	return (0);
+}
