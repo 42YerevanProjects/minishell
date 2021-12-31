8c30489762e4cb0a9eb28d4ee3d54b0f41061058
Nathanael <sargishovsepyan2001@gmail.com>
2021-12-31 12:12:34 -0500

    A function that mimics the beavior of the command pwd in bash.

---
+++ srcs/builtins/ft_pwd.c
@@ -0,0 +1,18 @@
+#include "../../includes/minishell.h"
+
+/**
+ * The function mimics the behavior of pwd command in bash. It returns 0
+ * on success and 1 otherwise.
+ **/
+
+int	ft_pwd(t_cmd *cmd)
+{
+	if (ft_matrixlen(cmd->args) > 1)
+	{
+		ft_minishell_errror("minishell: pwd: Invalid number of args to pwd");
+		return (1);
+	}
+	ft_putstr_fd(ft_getenv("PWD"), cmd->out);
+	ft_putstr_fd("\n", cmd->out);
+	return (0);
+}
