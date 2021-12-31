adb3174943d2a674b480cbc32f77bdd02bdb0ae8
Nathanael <sargishovsepyan2001@gmail.com>
2021-12-31 12:09:48 -0500

    Given a string check if it is a number

---
+++ utils/ft_isnum.c
@@ -0,0 +1,17 @@
+#include "../includes/minishell.h"
+
+int	ft_isnum(char *num)
+{
+	int	i;
+
+	i = 0;
+	if (num[0] == '-')
+		i++;
+	while (num[i])
+	{
+		if (!ft_isdigit(num[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
