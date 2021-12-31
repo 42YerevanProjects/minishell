429d7013f0eccc2c1366b87950fbd1d6ea41b068
Nathanael <sargishovsepyan2001@gmail.com>
2021-12-31 12:08:44 -0500

    Prints the environment variables with values in the double quotes

---
+++ srcs/env/ft_print_env.c
@@ -0,0 +1,26 @@
+#include "../../includes/minishell.h"
+
+/**
+ * The function outputs the environment. I wraps the values
+ * of the environment variables with double quotes.
+ **/
+
+void	ft_print_env(int fd)
+{
+	t_env	*head;
+	int		i;
+
+	head = g_mini.env;
+	while (head)
+	{
+		i = 0;
+		while (head->value[i] != '=')
+			ft_putchar_fd(head->value[i++], fd);
+		ft_putstr_fd("=\"", fd);
+		i++;
+		while (head->value[i])
+			ft_putchar_fd(head->value[i++], fd);
+		ft_putstr_fd("\"\n", fd);
+		head = head->next;
+	}
+}
