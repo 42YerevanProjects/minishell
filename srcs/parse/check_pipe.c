272e9696264de6df0dd1e1b4fa7213a8605f5f1e
Nathanael <sargishovsepyan2001@gmail.com>
2021-12-31 12:02:06 -0500

    Added some comments

--- srcs/parse/check_pipe.c
+++ srcs/parse/check_pipe.c
@@ -1,5 +1,10 @@
 #include "../../includes/minishell.h"
 
+/**
+ * The functrion check for a valid pipe. If the pipe has second
+ * part or it is not a double pipe it returns 1, 0 otherwise. 
+ **/
+
 static int	valid_pipe(int index)
 {
 	char *token;
@@ -11,6 +16,12 @@ static int	valid_pipe(int index)
 			return (1);
 	return (0);
 }
+
+/**
+ * The function checks for invalid pipe. If the pipe has no second
+ * part (command | NULL) or it is a double pipe (`||`) it returns
+ * 1, 0 otherwise.
+ **/
 
 static int	not_valid_pipe(int index)
 {
@@ -24,12 +35,20 @@ static int	not_valid_pipe(int index)
 	return (0);
 }
 
+/**
+ * The function check for valid pipe. If the pipe is valid => it just
+ * increments the index by passing to the next token. Else if the pipe
+ * is not valid (it is a `||` or a pipe without second part) it outputs 
+ * error.
+ **/
+
 int	check_pipe(char **args, int *index)
 {
 	if (valid_pipe(*index))
 		*index = *index + 1;
 	else if (not_valid_pipe(*index))
 	{
+		ft_minishell_error("minishell: syntax error: pipe");
 		ft_free_matrix(args);
 		g_mini.status = SYNTAX_ERROR;
 		return (1);
