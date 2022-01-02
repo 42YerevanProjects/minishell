97bebf2107c8fa9e445eb98ba7b173ca41ab5c63
Nathanael <sargishovsepyan2001@gmail.com>
2022-01-02 11:37:35 -0500

    Added get_name function to make the var name comparison correct and added a check for prev being NULL

--- srcs/env/ft_remove_from_env.c
+++ srcs/env/ft_remove_from_env.c
@@ -1,5 +1,25 @@
 #include "../../includes/minishell.h"
 
+static char	*get_name(char *var)
+{
+	char	*name;
+	int		i;
+
+	i = 0;
+	while (var[i] && var[i] != '=')
+		i++;
+	name = (char *)malloc(sizeof(char) * i + 2);
+	i = 0;
+	while (var[i] && var[i] != '=')
+	{
+		name[i] = var[i];
+		i++;
+	}
+	name[i] = '=';
+	name[i + 1] = '\0';
+	return (name);
+}
+
 void	ft_remove_from_env(char *var)
 {
 	t_env	*head;
@@ -9,9 +29,10 @@ void	ft_remove_from_env(char *var)
 	prev = NULL;
 	while (head)
 	{
-		if (!ft_strcmp(head->value, var))
+		if (!ft_strcmp(get_name(head->value), get_name(var)))
 		{
-			prev->next = head->next;
+			if (prev != NULL)
+				prev->next = head->next;
 			head->next = NULL;
 			head = NULL;
 			break ;
