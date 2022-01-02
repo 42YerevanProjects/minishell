ef87bb34e9daacfb084f41f497d35cfd29d8dbf0
Nathanael <sargishovsepyan2001@gmail.com>
2022-01-02 10:20:03 -0500

    Removed an unneccessary if check

--- srcs/builtins/ft_pwd.c
+++ srcs/builtins/ft_pwd.c
@@ -7,11 +7,6 @@
 
 int	ft_pwd(t_cmd *cmd)
 {
-	if (ft_matrixlen(cmd->args) > 1)
-	{
-		ft_minishell_error("minishell: pwd: Too many args to pwd");
-		return (1);
-	}
 	ft_putstr_fd(ft_getenv("PWD"), cmd->out);
 	ft_putstr_fd("\n", cmd->out);
 	return (0);
