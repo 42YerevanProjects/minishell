35c2e6e3c7ba3f96417c8c9fb0acac5fdeb997c7
Nathanael <sargishovsepyan2001@gmail.com>
2022-01-02 11:31:09 -0500

    Added equal sign after an added env var if needed

--- srcs/env/ft_add_to_env.c
+++ srcs/env/ft_add_to_env.c
@@ -1,10 +1,26 @@
 #include "../../includes/minishell.h"
 
+static int	ft_contains_equal(char *var)
+{
+	int i;
+
+	i = 0;
+	while (var[i])
+	{
+		if (var[i] == '=')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
 void	ft_add_to_env(char *var)
 {
 	t_env	*head;
 
 	head = g_mini.env;
+	if (!ft_contains_equal(var))
+		var = ft_strjoin(var, "=");
 	while (head)
 	{
 		if (head->next == NULL)
